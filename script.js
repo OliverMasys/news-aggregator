a24bb88faa5de6d0fb161eb356082f79467eecfb
Stephen Herd <sherd@dallascollege.edu>
2024-08-11 14:25:08 -0500

    Init commit

---
+++ script.js
@@ -0,0 +1,95 @@
+// Singleton Pattern: ConfigManager
+const ConfigManager = (function() {
+    let instance;
+
+    function createInstance() {
+        return {
+            theme: 'dark',
+            apiUrl: 'https://newsapi.org/v2/top-headlines',
+            apiKey: 'your_api_key_here' // TODO: Replace with your NewsAPI key
+        };
+    }
+
+    // TODO: Return getInstance function
+
+
+
+
+
+
+
+
+})();
+
+// Module Pattern: NewsFetcher
+const NewsFetcher = (function () {
+    // TODO: Create config object with getInstance of ConfigManager
+
+    
+    function fetchArticles() {
+        // TODO: return fetch data adjusted for only articles
+
+
+
+    }
+
+    return {
+        getArticles: fetchArticles
+    };
+})();
+
+// Observer Pattern: NewsFeed
+function NewsFeed() {
+    this.observers = [];
+    this.articles = [];
+}
+
+// TODO: Create NewsFeed prototype
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Instantiate the NewsFeed
+const newsFeed = new NewsFeed();
+
+// Observer 1: Update Headline
+function updateHeadline(article) {
+    const headlineElement = document.getElementById('headline').querySelector('p');
+    headlineElement.textContent = article.title;
+}
+
+// Observer 2: Update Article List
+function updateArticleList(article) {
+    const articleListElement = document.getElementById('articles');
+    const listItem = document.createElement('li');
+    listItem.textContent = article.title;
+    articleListElement.appendChild(listItem);
+}
+
+// TODO: Subscribe Observers
+
+
+
+// Fetch and display articles
+NewsFetcher.getArticles().then(articles => {
+    articles.forEach(article => {
+        newsFeed.addArticle(article);
+    });
+});
+
+// Display Config Info
+const configInfo = ConfigManager.getInstance();
+document.getElementById('configInfo').textContent = `Theme: ${configInfo.theme}`;
+
